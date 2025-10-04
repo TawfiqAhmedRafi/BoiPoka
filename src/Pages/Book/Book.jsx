0a5438e89aaa96ff6942c7624142453b796f8a5e
TawfiqAhmedRafi <rafi70722@gmail.com>
2025-10-04 23:56:38 +0600

    Dynamic Routes

--- src/Pages/Book/Book.jsx
+++ src/Pages/Book/Book.jsx
@@ -1,30 +1,41 @@
-import React, { use } from 'react';
+import React from 'react';
 import { FaRegStarHalfStroke } from "react-icons/fa6";
-
+import { Link } from "react-router";
 const Book = ({singleBook}) => {
     // const data = use(bookPromise)
      //console.log(singleBook)
-    const {bookName,author , image,rating,category}=singleBook
+    const {bookId,bookName,author , image,rating,category,tags,yearOfPublishing,publisher}=singleBook
 
     return (
-       <div className="card bg-base-100 w-96 shadow-sm border p-4">
+    <Link to={`/bookDetails/${bookId}`}>
+           <div className="card bg-base-100 w-96 shadow-sm border p-4 ">
   <figure className='p-4 bg-gray-100 w-11/12 mx-auto rounded-xl '>
     <img className='h-[166px]'
       src={image}
       alt="Shoes" />
   </figure>
   <div className="card-body">
+    <div className='flex justify-center gap-10 text-green-500'>
+        {
+        tags.map(tag=><button>{tag}</button> )
+    }
+    </div>
     <h2 className="card-title">
       {bookName}
-      <div className="badge badge-secondary">NEW</div>
+      <div className="badge badge-secondary">{yearOfPublishing}</div>
     </h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <div className="badge badge-outline">{category}</div>
-      <div className="badge badge-outline">{rating}<FaRegStarHalfStroke /></div>
+    <p> By : {author}</p>
+    <p>Published by :{publisher} </p>
+    <div className='border-t-1 border-dashed'>
+
+    </div>
+    <div className="card-actions flex justify-between ">
+      <div className="badge ">{category}</div>
+      <div className="badge ">{rating}<FaRegStarHalfStroke /></div>
     </div>
   </div>
 </div>
+    </Link>
     );
 };
 
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,8 @@ import { createBrowserRouter } from "react-router";
 import Root from '../Pages/Root/Root';
 import ErrorPage from '../Pages/ErrorPage/ErrorPage';
 import Home from '../Pages/Home/Home';
+import About from '../Pages/About/About';
+import BookDetails from '../Pages/bookDetails/BookDetails';
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -14,6 +16,16 @@ export const router = createBrowserRouter([
             loader:()=>fetch('booksData.json'),
             path : "/",
             Component : Home
+        },
+        {
+            path : '/about',
+            Component: About
+        },
+        {
+            path : '/bookDetails/:id',
+            // Component : BookDetails
+            element : <BookDetails />
+
         }
     ]
   }
